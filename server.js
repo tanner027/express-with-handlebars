91201f49aac577ab5fdbfbd2242fee575f900de1
Curtis Couture <ccouture@sitespect.com>
2021-11-18 10:48:37 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { engine } from "express-handlebars"; // "express-handlebars"
+
+import * as path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", path.resolve(__dirname, "./views"));
+
+app.get("/", (req, res) => {
+	res.render("home", {
+		name: (req.get("Experience") ? "Jim" : "Steven"),
+	});
+});
+
+app.listen(3000, () => {
+	console.log("express-handlebars example server listening on: 3000");
+});
